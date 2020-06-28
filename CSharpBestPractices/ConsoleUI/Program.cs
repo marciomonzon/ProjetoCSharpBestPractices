2b561ee14a5f5e0afa10a3b2aafc00e31d998cd5
Marcio <marcio.pcmonzon@gmail.com>
2020-06-28 13:16:59 -0300

    keep it simple

--- CSharpBestPractices/ConsoleUI/Program.cs
+++ CSharpBestPractices/ConsoleUI/Program.cs
@@ -1,15 +1,42 @@
-﻿using System;
+﻿using HelperLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+// Capturing a number os users of people's names
+// Loop through and say hi to them.
 namespace ConsoleUI
 {
     class Program
     {
+        private static List<Person> people = new List<Person>();
+
         static void Main(string[] args)
         {
+            // 4º passo
+            SetUpData();
+            LoopThrougData();
+        }
+
+        private static void SetUpData()
+        {
+            people.Add(new Person { FirstName = "Marcio", LastName = "Monzon" });
+            people.Add(new Person { FirstName = "Jose", LastName = "Monzon" });
+            people.Add(new Person { FirstName = "Garcia", LastName = "Monzon" });
+            people.Add(new Person { FirstName = "Tio", LastName = "Monzon" });
+        }
+
+        private static void LoopThrougData()
+        {
+            foreach (var person in people)
+            {
+                Console.WriteLine($"Hello {person.FirstName } { person.LastName }");
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ CSharpBestPractices/HelperLibrary/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperLibrary
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    //// má pratica
+    //public class Guest
+    //{
+
+    //}
+}
